10a0fe164ff06dcb048ee8173dbeb78b09fabfd6
AdautoMarques <adautomarques937@gmail.com>
2023-07-25 07:14:46 -0300

    first commit

---
+++ src/app/controllers/CustomersController.js
@@ -0,0 +1,18 @@
+
+class CustomersController {
+    index(req,res){
+
+    }
+    show(req,res){
+        
+    }
+    create(req,res){
+        
+    }
+    update(req,res){
+        
+    }
+    destroy(req,res){
+        
+    }
+}
